6b74f47f5a83cd72027ee52a22c4991197a4ede3
Noam shushan <noam8shu@gmail.com>
2020-10-20 19:03:06 +0300

    סידרתי שורות

--- dotNet5781_7588_3756/dotNet5781_00_7588_3756/Program3756.cs
+++ dotNet5781_7588_3756/dotNet5781_00_7588_3756/Program3756.cs
@@ -4,12 +4,11 @@
 namespace dotNet5781_00_7588_3756
 {
        partial class Program
-        {
-             static partial void welcome3756()
+       {
+            static partial void welcome3756()
             {
-
                 Console.WriteLine("I am also here");
             }
-        }
+       }
     
 }
