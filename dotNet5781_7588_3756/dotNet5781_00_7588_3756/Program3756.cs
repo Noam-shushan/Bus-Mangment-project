f4933192bbe44abe0172ed81451daf9af0730f86
elihau bella <elihau@g.jct.ac.il>
2020-10-20 18:54:15 +0300

    welcom3756

---
+++ dotNet5781_7588_3756/dotNet5781_00_7588_3756/Program3756.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+
+namespace dotNet5781_00_7588_3756
+{
+       partial class Program
+        {
+             static partial void welcome3756()
+            {
+
+                Console.WriteLine("I am also here");
+            }
+        }
+    
+}
