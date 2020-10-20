4b2e32705858ff781e108094fb1241b9d1e554be
Noam shushan <noam8shu@gmail.com>
2020-10-20 14:51:09 +0300

    add the welcome7588() function

---
+++ dotNet5781_7588_3756/dotNet5781_00_7588_3756/Program7588.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+
+namespace dotNet5781_00_7588_3756
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            welcome7588();
+            Console.ReadKey();
+        }
+
+        private static void welcome7588()
+        {
+            Console.WriteLine("Enter your name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("{0}, welcome to my first console application", name);
+        }
+    }
+}
