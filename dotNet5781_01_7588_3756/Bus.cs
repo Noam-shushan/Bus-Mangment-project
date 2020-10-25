52b05e105ee9e433f759e064cd8be1045d46d3de
Noam shushan <noam8shu@gmail.com>
2020-10-26 00:23:56 +0200

    Refactor solution structure and  fix ex01 project file

---
+++ dotNet5781_01_7588_3756/Bus.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+public class Bus
+{
+	private int _licenseNumber;
+	private DateTime _startActivity;
+	private int _mileage;
+	private DateTime _lastTreatment;
+	private const int MAX_KILOMETER_AFTER_REFUELING = 1200
+
+	public Bus(int licenseNumber, DateTime startActivity)
+	{
+		this._licenseNumber = licenseNumber;
+		this._startActivity = startActivity;
+		this._mileage = 0;
+	}
+
+	public int Mileage
+    {
+        get { return this._mileage }
+		set { this._mileage = newMileage}
+    }
+
+	public void Treatment()
+    {
+		this._lastTreatment = DateTime.Now;
+    }
+
+	//private bool NeedsTreatment()
+    //{
+		//return this._lastTreatment
+
+	//}
+
+
+
+}
---
+++ dotNet5781_01_7588_3756/Main.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace dotNet5781_01_7588_3756
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //DateTime d = new DateTime(2018, 10, 24);
+            //Console.WriteLine((DateTime.Now - d).TotalDays < 365);
+            Console.WriteLine("hi");
+        }
+    }
+}
