2ef2769901e2ab7063e03be879d8946fb5fe3eef
Noam shushan <noam8shu@gmail.com>
2020-10-26 02:00:06 +0200

    create most of the mathods to the Bus class

--- dotNet5781_01_7588_3756/Bus.cs
+++ dotNet5781_01_7588_3756/Bus.cs
@@ -6,31 +6,56 @@ public class Bus
 	private DateTime _startActivity;
 	private int _mileage;
 	private DateTime _lastTreatment;
-	private const int MAX_KILOMETER_AFTER_REFUELING = 1200
+	private bool _isFueled;
+	private const int KILOMETER_BEFORE_TREATMENT = 20000;
+	private const int MAX_KILOMETER_AFTER_REFUELING = 1200;
 
 	public Bus(int licenseNumber, DateTime startActivity)
 	{
-		this._licenseNumber = licenseNumber;
-		this._startActivity = startActivity;
-		this._mileage = 0;
+		_licenseNumber = licenseNumber;
+		_startActivity = startActivity;
+		_mileage = 0;
 	}
+
+	public int LiscenseNumber
+    {
+		get => _licenseNumber;
+    }
 
 	public int Mileage
     {
-        get { return this._mileage }
-		set { this._mileage = newMileage}
+		get => _mileage;
+		/*אם הקילומטרז + הנסיעה החדשה קטן מ1200 
+		 * וגם הוא תידלק
+		 * וגם הוא לא צריך טיפול (כי עברה שנה או 20000 קלימטר)
+		 * אז הוא יכול לקבל את הנסיעה החדשה
+		 */
+		set { if (IsFueled && !this.NeedsTreatment())
+				_mileage = value; } 
     }
+
+	public bool IsFueled
+    {
+		get => _isFueled;
+		set => _isFueled = value;
+    }
+
+	public bool NeedRefueling(int kilomters)
+    {
+		return _mileage + kilomters < MAX_KILOMETER_AFTER_REFUELING;
+
+	}
 
 	public void Treatment()
     {
-		this._lastTreatment = DateTime.Now;
+		_lastTreatment = DateTime.Now;
     }
 
-	//private bool NeedsTreatment()
-    //{
-		//return this._lastTreatment
-
-	//}
+	private bool NeedsTreatment()
+    {
+		return (this._lastTreatment - DateTime.Now.Date).Days >= 365 ||
+			_mileage >= KILOMETER_BEFORE_TREATMENT;
+	}
 
 
 
--- dotNet5781_01_7588_3756/Main.cs
+++ dotNet5781_01_7588_3756/Main.cs
@@ -6,9 +6,6 @@ namespace dotNet5781_01_7588_3756
     {
         static void Main(string[] args)
         {
-            //DateTime d = new DateTime(2018, 10, 24);
-            //Console.WriteLine((DateTime.Now - d).TotalDays < 365);
-            Console.WriteLine("hi");
         }
     }
 }
